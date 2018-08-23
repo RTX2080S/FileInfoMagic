18caa638f37df56f7e9c49ecaec8513778a18b7c
cgloier <cgloier@163.com>
2018-08-23 13:50:05 +0800

    Clean up Deterministic attribute

--- Src/FileInfoMagic.Services/Properties/AssemblyInfo.cs
+++ Src/FileInfoMagic.Services/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileInfoMagic.Services")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FileInfoMagic Services")]
+[assembly: AssemblyDescription("FileInfoMagic Services")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FileInfoMagic.Services")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Alienlab")]
+[assembly: AssemblyProduct("FileInfoMagic")]
+[assembly: AssemblyCopyright("Copyright © Alien Laboratory 2018")]
+[assembly: AssemblyTrademark("ALI")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -31,6 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
