7c777df286e4e41691425bec5ab83ccd6e0288fd
cgloier <cgloier@163.com>
2018-08-23 15:14:11 +0800

    Implement textbox validation

---
+++ Src/FileInfoMagic/ValidationRules/DateTimeValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace FileInfoMagic.ValidationRules
+{
+    public class DateTimeValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var dateTimeStr = value as string;
+            var result = DateTime.TryParse(dateTimeStr, out DateTime dateTimeResult);
+            return new ValidationResult(result, null);
+        }
+    }
+}
