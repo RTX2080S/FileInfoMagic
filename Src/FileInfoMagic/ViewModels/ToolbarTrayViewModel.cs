b8847301f2c3d061c63f3d21ebecce3b822080f5
LL <52593730@qq.com>
2018-08-24 22:28:47 +0800

    Create toolbar

---
+++ Src/FileInfoMagic/ViewModels/ToolbarTrayViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WinForm = System.Windows.Forms;
+using Alienlab.WPF.Helpers;
+using FileInfoMagic.Infrastructure.ViewModels;
+using System.Text;
+
+namespace FileInfoMagic.ViewModels
+{
+    public class ToolbarTrayViewModel : CommonViewModel
+    {
+        private ICommand aboutCommand;
+
+        private void executeAboutCommand()
+        {
+            var aboutContent = new StringBuilder();
+            aboutContent.AppendLine($"{WinForm.Application.CompanyName} {WinForm.Application.ProductName}");
+            aboutContent.AppendLine($"Version {WinForm.Application.ProductVersion}");
+            aboutContent.AppendLine($"By {WinForm.Application.CompanyName}");
+            aboutContent.AppendLine("All rights reserved.");
+            MessageBox.Show($"{aboutContent.ToString()}", $"About {WinForm.Application.ProductName}",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public ICommand AboutCommand
+        {
+            get
+            {
+                aboutCommand = aboutCommand ?? new RelayCommand(param => executeAboutCommand());
+                return aboutCommand;
+            }
+        }
+    }
+}
---
+++ Src/FileInfoMagic/Views/ToolbarTrayView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FileInfoMagic.Views
+{
+    /// <summary>
+    /// Interaction logic for ToolbarTrayView.xaml
+    /// </summary>
+    public partial class ToolbarTrayView : UserControl
+    {
+        public ToolbarTrayView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Remove Toolbar OverflowGrid
+        /// <see cref="https://stackoverflow.com/questions/1050953/wpf-toolbar-how-to-remove-grip-and-overflow/1051264#1051264" />
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            var toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+                overflowGrid.Visibility = Visibility.Collapsed;
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+                mainPanelBorder.Margin = new Thickness();
+        }
+    }
+}
